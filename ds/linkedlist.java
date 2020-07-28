6db5dd43de6d3dc66afddc64eeeb6e711bfc50a6
Kashish2041 <68331815+Kashish2041@users.noreply.github.com>
2020-07-28 18:02:29 +0530

    Create linkedlist.java

---
+++ ds/linkedlist.java
@@ -0,0 +1,79 @@
+package ds;
+public class linkedlist
+{
+    Node head;
+    void putAtFirst(int data)
+    {
+        Node newnode=new Node(data);
+        newnode.next=head;
+        head=newnode;
+    }
+    
+    void insert(Node prevnode,int data)
+    {
+        if(prevnode==null)
+        {
+            System.out.print("the given previous note cannot be null");
+            return;
+        }
+        Node newnode=new Node(data);
+        newnode.next=prevnode.next;
+        prevnode.next=newnode;
+    }
+    
+    void add(int data)
+    {
+        Node toAdd=new Node(data);
+        if(isEmpty())
+        {
+            head=toAdd;
+            return;
+        }
+        else
+        {
+            Node last=head;
+            while(last.next!=null)
+            {
+                last=last.next;    
+            }
+            last.next=toAdd;
+        }
+    }
+   
+     void print()
+     {
+         Node last=head;
+            while(last!=null)
+            {
+                System.out.print(last.data +" ");
+                last=last.next;
+            } 
+     }
+      boolean isEmpty()
+	{
+		return head == null;
+	}
+    static class Node
+    {
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data=data;
+            next=null;
+        }
+    }
+	public static void main(String[] args) 
+	{
+	    Linkedlist myll=new Linkedlist();
+	    myll.add(52);
+	    myll.add(2);
+	    myll.add(42);
+	    myll.add(32);
+	    myll.add(22);
+	    myll.putAtFirst(12);
+	    myll.insert(myll.head.next,8);
+	    myll.print();
+	    
+	}
+}
